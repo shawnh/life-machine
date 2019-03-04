42a299b99a8263c6891306ed3138651ad2f37660
Shawn Hamman <shawn@bitsparks.com>
2019-03-04 20:25:38 +1300

    Initial Commit

---
+++ Life Machine/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Life Machine
+//
+//  Created by Shawn Hamman on 4/03/19.
+//  Copyright © 2019 Shawn Hamman. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
